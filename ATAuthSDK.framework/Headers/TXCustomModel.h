384a23101ecdf4e090440ac9a7989e1c3333b131
litianqi <litianqi@joyy.com>
2022-10-29 13:27:18 +0800

    feat: 更新2.12.9

--- ATAuthSDK.framework/Headers/TXCustomModel.h
+++ ATAuthSDK.framework/Headers/TXCustomModel.h
@@ -3,7 +3,7 @@
 //  ATAuthSDK
 //
 //  Created by yangli on 2019/4/4.
-//  Copyright © 2019 alicom. All rights reserved.
+//  Copyright © 2019. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
@@ -37,7 +37,7 @@ typedef CGRect(^PNSBuildFrameBlock)(CGSize screenSize, CGSize superViewSize, CGR
 #pragma mark- 全屏、弹窗模式设置
 /**
  *  授权页面中，渲染并显示所有控件的view，称content view，不实现该block默认为全屏模式
- *  实现弹窗的方案 x > 0 || y > 0 width < 屏幕宽度 || height < 屏幕高度
+ *  实现弹窗的方案 x >= 0 || y >= 0 width <= 屏幕宽度 || height <= 屏幕高度
  */
 @property (nonatomic, copy) PNSBuildFrameBlock contentViewFrameBlock;
 
@@ -50,6 +50,10 @@ typedef CGRect(^PNSBuildFrameBlock)(CGSize screenSize, CGSize superViewSize, CGR
 @property (nonatomic, strong) UIColor *alertBlurViewColor;
 /** 底部蒙层背景透明度，默认0.5 */
 @property (nonatomic, assign) CGFloat alertBlurViewAlpha;
+/** contentView背景颜色，默认白色 */
+@property (nonatomic, strong) UIColor *alertContentViewColor;
+/** contentView背景透明度，默认1.0 ，即不透明*/
+@property (nonatomic, assign) CGFloat alertContentViewAlpha;
 /** contentView的四个圆角值，顺序为左上，左下，右下，右上，需要填充4个值，不足4个值则无效，如果值<=0则为直角 */
 @property (nonatomic, copy) NSArray<NSNumber *> *alertCornerRadiusArray;
 /** 标题栏背景颜色 */
@@ -71,8 +75,10 @@ typedef CGRect(^PNSBuildFrameBlock)(CGSize screenSize, CGSize superViewSize, CGR
 @property (nonatomic, copy) PNSBuildFrameBlock alertCloseItemFrameBlock;
 
 #pragma mark- 导航栏（只对全屏模式有效）
-/**导航栏是否隐藏*/
+/**授权页显示中，导航栏是否隐藏，默认NO*/
 @property (nonatomic, assign) BOOL navIsHidden;
+/**授权页push到其他页面后，导航栏是否隐藏，默认NO*/
+@property (nonatomic, assign) BOOL navIsHiddenAfterLoginVCDisappear;
 /** 导航栏主题色 */
 @property (nonatomic, strong) UIColor *navColor;
 /** 导航栏标题，内容、字体、大小、颜色 */
@@ -93,15 +99,35 @@ typedef CGRect(^PNSBuildFrameBlock)(CGSize screenSize, CGSize superViewSize, CGR
 
 #pragma mark- 全屏、弹窗模式共同属性
 
-#pragma mark- 授权页弹出方向
-/** 授权页弹出方向，默认PNSPresentationDirectionBottom */
+#pragma mark- 授权页动画相关
+/** 授权页弹出方向，默认PNSPresentationDirectionBottom，该属性只对自带动画起效，不影响自定义动画 */
 @property (nonatomic, assign) PNSPresentationDirection presentDirection;
+/** 授权页显示和消失动画时间，默认为0.25s，<= 0 时关闭动画，该属性只对自带动画起效，不影响自定义动画 **/
+@property (nonatomic, assign) CGFloat animationDuration;
+
+/** 授权页显示动画（弹窗 & 全屏），不设置或设置为nil默认使用自带动画，SDK内部会主动更改动画的一些属性（包括：removedOnCompletion = NO、fillMode = kCAFillModeForwards 及 delegate） **/
+@property (nonatomic, strong, nullable) CAAnimation *entryAnimation;
+/** 授权页消失动画（弹窗 & 全屏），不设置或设置为nil默认使用自带动画，SDK内部会主动更改动画的一些属性（包括：removedOnCompletion = NO、fillMode = kCAFillModeForwards 及 delegate） **/
+@property (nonatomic, strong, nullable) CAAnimation *exitAnimation;
+
+/** 授权页显示时的背景动画（仅弹窗），不设置或设置为nil默认使用自带动画，SDK内部会主动更改动画的一些属性（包括：removedOnCompletion = NO、fillMode = kCAFillModeForwards 及 delegate） **/
+@property (nonatomic, strong, nullable) CAAnimation *bgEntryAnimation;
+/** 授权页消失时的背景动画（仅弹窗），不设置或设置为nil默认使用自带动画，SDK内部会主动更改动画的一些属性（包括：removedOnCompletion = NO、fillMode = kCAFillModeForwards 及 delegate） **/
+@property (nonatomic, strong, nullable) CAAnimation *bgExitAnimation;
 
 #pragma mark- 状态栏
 /** 状态栏是否隐藏，默认NO */
 @property (nonatomic, assign) BOOL prefersStatusBarHidden;
 /** 状态栏主题风格，默认UIStatusBarStyleDefault */
 @property (nonatomic, assign) UIStatusBarStyle preferredStatusBarStyle;
+
+#pragma mark- 背景
+/** 授权页背景色 */
+@property (nonatomic, strong) UIColor *backgroundColor;
+/** 授权页背景图片 */
+@property (nonatomic, strong) UIImage *backgroundImage;
+/** 授权页背景图片view的 content mode，默认为 UIViewContentModeScaleAspectFill */
+@property (nonatomic, assign) UIViewContentMode backgroundImageContentMode;
 
 #pragma mark- logo图片
 /** logo图片设置 */
@@ -163,7 +189,7 @@ typedef CGRect(^PNSBuildFrameBlock)(CGSize screenSize, CGSize superViewSize, CGR
 @property (nonatomic, assign) BOOL autoHideLoginLoading;
 /**
  *  构建登录按钮的frame，view布局或布局发生变化时调用，不实现则按默认处理
- *  注：不能超出父视图 content view，height不能小于40，width不能小于父视图宽度的一半
+ *  注：不能超出父视图 content view，height不能小于20，width不能小于父视图宽度的一半
  */
 @property (nonatomic, copy) PNSBuildFrameBlock loginBtnFrameBlock;
 /**
@@ -171,7 +197,7 @@ typedef CGRect(^PNSBuildFrameBlock)(CGSize screenSize, CGSize superViewSize, CGR
  *  注：设置超出父视图 content view 时不生效
  */
 @property (nonatomic, assign) CGFloat loginBtnTopOffetY DEPRECATED_MSG_ATTRIBUTE("Please use loginBtnFrameBlock instead");
-/** 登录按钮高度，小于40.0pt不生效，不设置则按默认处理 */
+/** 登录按钮高度，小于20.0pt不生效，不设置则按默认处理 */
 @property (nonatomic, assign) CGFloat loginBtnHeight DEPRECATED_MSG_ATTRIBUTE("Please use loginBtnFrameBlock instead");
 /** 登录按钮相对content view的左右边距，按钮宽度必须大于等于屏幕的一半，不设置则按默认处理 */
 @property (nonatomic, assign) CGFloat loginBtnLRPadding DEPRECATED_MSG_ATTRIBUTE("Please use loginBtnFrameBlock instead");
@@ -179,6 +205,8 @@ typedef CGRect(^PNSBuildFrameBlock)(CGSize screenSize, CGSize superViewSize, CGR
 #pragma mark- 协议
 /** checkBox图片组，[uncheckedImg,checkedImg]*/
 @property (nonatomic, copy) NSArray<UIImage *> *checkBoxImages;
+/** checkBox图片距离控件边框的填充，默认为 UIEdgeInsetsZero，确保控件大小减去内填充大小为资源图片大小情况下，图片才不会变形 **/
+@property (nonatomic, assign) UIEdgeInsets checkBoxImageEdgeInsets;
 /** checkBox是否勾选，默认NO */
 @property (nonatomic, assign) BOOL checkBoxIsChecked;
 /** checkBox是否隐藏，默认NO */
@@ -192,9 +220,11 @@ typedef CGRect(^PNSBuildFrameBlock)(CGSize screenSize, CGSize superViewSize, CGR
 @property (nonatomic, copy) NSArray<NSString *> *privacyTwo;
 /** 协议3，[协议名称,协议Url]，注：三个协议名称不能相同 */
 @property (nonatomic, copy) NSArray<NSString *> *privacyThree;
+/** 协议名称之间连接字符串数组，默认 ["和","、","、"] ，即第一个为"和"，其他为"、"，按顺序读取，为空则取默认 */
+@property (nonatomic, copy) NSArray<NSString *> *privacyConectTexts;
 /** 协议内容颜色数组，[非点击文案颜色，点击文案颜色] */
 @property (nonatomic, copy) NSArray<UIColor *> *privacyColors;
-/** 协议文案支持居中、居左设置，默认居左 */
+/** 协议文案支持居中、居左、居右设置，默认居左 */
 @property (nonatomic, assign) NSTextAlignment privacyAlignment;
 /** 协议整体文案，前缀部分文案 */
 @property (nonatomic, copy) NSString *privacyPreText;
@@ -204,8 +234,12 @@ typedef CGRect(^PNSBuildFrameBlock)(CGSize screenSize, CGSize superViewSize, CGR
 @property (nonatomic, copy) NSString *privacyOperatorPreText;
 /** 运营商协议名称后缀文案，仅支持 >)]》）】』*/
 @property (nonatomic, copy) NSString *privacyOperatorSufText;
+/** 运营商协议指定显示顺序，默认0，即第1个协议显示，最大值可为3，即第4个协议显示*/
+@property (nonatomic, assign) NSInteger privacyOperatorIndex;
 /** 协议整体文案字体大小，小于12.0不生效 */
 @property (nonatomic, strong) UIFont *privacyFont;
+/** checkBox是否扩大按钮可交互范围至"协议前缀部分文案(默认:我已阅读并同意)"区域，默认NO */
+@property (nonatomic, assign) BOOL expandAuthPageCheckedScope;
 
 /**
  *  构建协议整体（包括checkBox）的frame，view布局或布局发生变化时调用，不实现则按默认处理
@@ -213,6 +247,10 @@ typedef CGRect(^PNSBuildFrameBlock)(CGSize screenSize, CGSize superViewSize, CGR
  *  最终会根据设置进来的width对协议文本进行自适应，得到的size是协议控件的最终大小
  */
 @property (nonatomic, copy) PNSBuildFrameBlock privacyFrameBlock;
+/**
+ *  未同意协议时点击登录按钮，协议整体文案的动画效果，不设置或设置为nil默认没有动画，SDK内部会主动更改动画的一些属性（包括：removedOnCompletion = NO、fillMode = kCAFillModeRemoved 及 delegate）
+ */
+@property (nonatomic, strong, nullable) CAAnimation *privacyAnimation;
 /** 协议整体相对屏幕底部的Y轴距离，与其他有区别！！不能小于0 */
 @property (nonatomic, assign) CGFloat privacyBottomOffetY DEPRECATED_MSG_ATTRIBUTE("Please use privacyFrameBlock instead");
 /** 协议整体（包括checkBox）相对content view的左右边距，当协议整体宽度小于（content view宽度-2*左右边距）且居中模式，则左右边距设置无效，不能小于0 */
@@ -230,6 +268,8 @@ typedef CGRect(^PNSBuildFrameBlock)(CGSize screenSize, CGSize superViewSize, CGR
 @property (nonatomic, assign) CGFloat changeBtnTopOffetY DEPRECATED_MSG_ATTRIBUTE("Please use changeBtnFrameBlock instead");
 
 #pragma mark- 协议详情页
+/** 协议详情页容器是否自定义，默认NO，若为YES，则根据 PNSCodeLoginControllerClickProtocol 返回码获取协议点击详情信息 */
+@property (nonatomic, assign) BOOL privacyVCIsCustomized;
 /** 导航栏背景颜色设置 */
 @property (nonatomic, strong) UIColor *privacyNavColor;
 /** 导航栏标题字体、大小 */
@@ -262,6 +302,69 @@ typedef CGRect(^PNSBuildFrameBlock)(CGSize screenSize, CGSize superViewSize, CGR
 */
 @property (nonatomic, copy) void(^customViewLayoutBlock)(CGSize screenSize, CGRect contentViewFrame, CGRect navFrame, CGRect titleBarFrame, CGRect logoFrame, CGRect sloganFrame, CGRect numberFrame, CGRect loginFrame, CGRect changeBtnFrame, CGRect privacyFrame);
 
+#pragma mark - 二次隐私协议弹窗设置
+/** 二次隐私协议弹窗是否需要显示, 默认NO */
+@property (nonatomic, assign) BOOL privacyAlertIsNeedShow;
+/** 二次隐私协议弹窗点击按钮是否需要执行登陆，默认YES */
+@property (nonatomic, assign) BOOL privacyAlertIsNeedAutoLogin;
+/** 二次隐私协议弹窗显示自定义动画，默认从下往上位移动画 */
+@property (nonatomic, strong, nullable) CAAnimation *privacyAlertEntryAnimation;
+/** 二次隐私协议弹窗隐藏自定义动画，默认从上往下位移动画 */
+@property (nonatomic, strong, nullable) CAAnimation *privacyAlertExitAnimation;
+/** 二次隐私协议弹窗的四个圆角值，顺序为左上，左下，右下，右上，需要填充4个值，不足4个值则无效，如果值<=0则为直角 ，默认0*/
+@property (nonatomic, copy) NSArray<NSNumber *> *privacyAlertCornerRadiusArray;
+/** 二次隐私协议弹窗背景颜色，默认为白色 */
+@property (nonatomic, strong) UIColor *privacyAlertBackgroundColor;
+/** 二次隐私协议弹窗透明度，默认不透明1.0 ，设置范围0.3~1.0之间 */
+@property (nonatomic, assign) CGFloat privacyAlertAlpha;
+/** 二次隐私协议弹窗标题文字大小，最小12，默认12 */
+@property (nonatomic, strong) UIFont *privacyAlertTitleFont;
+/** 二次隐私协议弹窗标题文字颜色，默认黑色 */
+@property (nonatomic, strong) UIColor *privacyAlertTitleColor;
+/** 二次隐私协议弹窗标题背景颜色，默认白色*/
+@property (nonatomic, strong) UIColor *privacyAlertTitleBackgroundColor;
+/** 二次隐私协议弹窗标题位置，默认居中*/
+@property (nonatomic, assign) NSTextAlignment privacyAlertTitleAlignment;
+/** 二次隐私协议弹窗协议内容文字大小，最小12，默认12 */
+@property (nonatomic, strong) UIFont *privacyAlertContentFont;
+/** 二次隐私协议弹窗协议内容背景颜色，默认白色 */
+@property (nonatomic, strong) UIColor *privacyAlertContentBackgroundColor;
+/** 二次隐私协议弹窗协议内容颜色数组，[非点击文案颜色，点击文案颜色],默认[0x999999,0x1890FF] */
+@property (nonatomic, copy) NSArray<UIColor *> *privacyAlertContentColors;
+/** 二次隐私协议弹窗协议文案支持居中、居左、居右设置，默认居左 */
+@property (nonatomic, assign) NSTextAlignment privacyAlertContentAlignment;
+/** 二次隐私协议弹窗按钮按钮背景图片 ,默认高度50.0pt，@[激活状态的图片,高亮状态的图片] */
+@property (nonatomic, strong) NSArray<UIImage *> *privacyAlertBtnBackgroundImages;
+/** 二次隐私协议弹窗按钮文字颜色，默认黑色, @[激活状态的颜色,高亮状态的颜色] */
+@property (nonatomic, copy) NSArray<UIColor *> *privacyAlertButtonTextColors;
+/** 二次隐私协议弹窗按钮文字大小，最小10，默认18*/
+@property (nonatomic, strong) UIFont *privacyAlertButtonFont;
+/** 二次隐私协议弹窗关闭按钮是否显示，默认显示 */
+@property (nonatomic, assign) BOOL privacyAlertCloseButtonIsNeedShow;
+/** 二次隐私协议弹窗右侧关闭按钮图片设置，默认内置的X图片*/
+@property (nonatomic, strong) UIImage *privacyAlertCloseButtonImage;
+/** 二次隐私协议弹窗背景蒙层是否显示 ,默认YES*/
+@property (nonatomic, assign) BOOL privacyAlertMaskIsNeedShow;
+/** 二次隐私协议弹窗点击背景蒙层是否关闭弹窗 ,默认YES*/
+@property (nonatomic, assign) BOOL tapPrivacyAlertMaskCloseAlert;
+/** 二次隐私协议弹窗蒙版背景颜色，默认黑色 */
+@property (nonatomic, strong) UIColor *privacyAlertMaskColor;
+/** 二次隐私协议弹窗蒙版透明度 设置范围0.3~1.0之间 ，默认0.5*/
+@property (nonatomic, assign) CGFloat privacyAlertMaskAlpha;
+/** 二次隐私协议弹窗蒙版显示动画，默认渐显动画 */
+@property (nonatomic, strong) CAAnimation *privacyAlertMaskEntryAnimation;
+/** 二次隐私协议弹窗蒙版消失动画，默认渐隐动画 */
+@property (nonatomic, strong) CAAnimation *privacyAlertMaskExitAnimation;
+/** 二次隐私协议弹窗尺寸设置,不能超出父视图 content view，height不能小于50，width不能小于0，默认屏幕居中，宽为屏幕的宽度减掉80，高为200 */
+@property (nonatomic, copy) PNSBuildFrameBlock privacyAlertFrameBlock;
+/** 二次隐私协议弹窗标题尺寸，默认x=0，y=0，width=弹窗宽度，最小宽度为100，height=根据文本计算的高度，最小高度为15，不能超出父视图 */
+@property (nonatomic, copy) PNSBuildFrameBlock privacyAlertTitleFrameBlock;
+/** 二次隐私协议弹窗内容尺寸，默认为从标题顶部位置开始，最终会根据设置进来的width对协议文本进行自适应，得到的size是协议控件的最终大小。不能超出父视图 */
+@property (nonatomic, copy) PNSBuildFrameBlock privacyAlertPrivacyContentFrameBlock;
+/** 二次隐私协议弹窗尺寸,默认为父视图的宽度一半,居中显示。高度默认50, */
+@property (nonatomic, copy) PNSBuildFrameBlock privacyAlertButtonFrameBlock;
+/** 二次隐私协议弹窗右侧关闭按钮尺寸，默认宽高44，居弹窗右侧15，居弹窗顶部0*/
+@property (nonatomic, copy) PNSBuildFrameBlock privacyAlertCloseFrameBlock;
 @end
 
 NS_ASSUME_NONNULL_END
